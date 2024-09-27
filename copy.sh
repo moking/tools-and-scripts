03e32e7593d4049ec10d9a3eb376dc24c8cc95b4
Fan Ni <fan.ni@samsung.com>
2024-09-27 11:27:52 -0700

    copy.sh: updated jumper configuration and rhost

--- copy.sh
+++ copy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-rhost='nifan@192.168.64.153'
-rhost='nifan@bgt140507bm02.dtc.local'
+jumper='nifan@149.97.161.244:9004'
+rhost='fan@deb-101020-bm01.dtc.local'
+
 
 if [ "$#" -lt 2 ];then
 	echo not enough parameters, exit
@@ -14,16 +15,16 @@ if [ "$1" == "-f" ];then
 		echo not enough parameters, exit
 		exit
 	fi
-	echo scp -r -J nifan@142.215.161.46:9002 $rhost:$2 $3
-	scp -r -J nifan@142.215.161.46:9002 $rhost:$2 $3
+	echo scp -r -J $jumper $rhost:$2 $3
+	scp -r -J $jumper $rhost:$2 $3
 elif [ "$1" == "-t" ];then
 	if [ "$#" -lt 3 ];then
 		echo not enough parameters, exit
 		exit
 	fi
-	echo scp -r -J nifan@142.215.161.46:9002 $2 $rhost:$3
-	scp -r -J nifan@142.215.161.46:9002 $2 $rhost:$3
+	echo scp -r -J $jumper $2 $rhost:$3
+	scp -r -J $jumper $2 $rhost:$3
 else
-	echo scp -r -J nifan@142.215.161.46:9002 $1 $rhost:$2
-	scp -r -J nifan@142.215.161.46:9002 $1 $rhost:$2
+	echo scp -r -J $jumper $1 $rhost:$2
+	scp -r -J $jumper $1 $rhost:$2
 fi
