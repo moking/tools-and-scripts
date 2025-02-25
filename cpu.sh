d0ad9e9094f2bc970e94320e3176592bb1a97289
Fan Ni <fan.ni@samsung.com>
2025-02-25 07:29:22 -0800

    9-irq high cpu usage

---
+++ cpu.sh
@@ -0,0 +1,4 @@
+echo "mask" > /sys/firmware/acpi/interrupts/gpe6F
+echo "mask" > /sys/firmware/acpi/interrupts/sci
+echo "disable" > /sys/firmware/acpi/interrupts/gpe6F
+echo "disable" > /sys/firmware/acpi/interrupts/sci
