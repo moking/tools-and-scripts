99c61c15bb04f11b27b7e8ae6726cdc0f493d103
Fan Ni <fan.ni@samsung.com>
2025-03-14 12:51:58 -0700

    kernel gdb fix

--- cpu.sh
+++ cpu.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 echo "mask" > /sys/firmware/acpi/interrupts/gpe6F
-echo "mask" > /sys/firmware/acpi/interrupts/sci
-echo "disable" > /sys/firmware/acpi/interrupts/gpe6F
-echo "disable" > /sys/firmware/acpi/interrupts/sci
+#echo "mask" > /sys/firmware/acpi/interrupts/sci
+#echo "disable" > /sys/firmware/acpi/interrupts/gpe6F
+#echo "disable" > /sys/firmware/acpi/interrupts/sci
